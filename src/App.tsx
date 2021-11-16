20c96393abe5e5180479feaca8bd7daa69fe7490
Julien Vallini <vallinij@gmail.com>
2021-11-16 17:25:04 +0100

    Extreme minimal app

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,43 @@
-import React, { useContext } from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import useRooms from 'hooks/useRooms';
-import { AppContext, DeviceProvider, AppProvider } from 'context';
-import RoomPicker from 'components/RoomPicker';
-import PseudoPicker from 'components/PseudoPicker';
-import AvatarPicker from 'components/AvatarPicker';
-import RoundPlaying from 'components/RoundPlaying';
-import { Device, GameState } from 'common/types';
+import useVoice from 'hooks/useVoice';
+import React from 'react';
+import { Button, SafeAreaView, Text } from 'react-native';
 
 const App = () => {
-  const rooms = useRooms();
-  const { session, game, patchGame, auth, connectToRoom, room, user } =
-    useContext(AppContext);
-
-  if (!room) {
-    return <RoomPicker />;
-  }
-
-  if (!auth || !user) return <Text>Loading</Text>;
-
-  if (!user.pseudo) return <PseudoPicker />;
-  if (!user.avatar) return <AvatarPicker />;
+  const { start, listening, stop, partialResults, results } = useVoice();
 
   return (
     <>
-      <Text>Current room : {room._id}</Text>
-      <Text>
-        Current user : {user.pseudo} {user.avatar}
-      </Text>
-      <Text>Current game : {game && game._id + ' - ' + game.state}</Text>
-      {game?.state === GameState.ROUND_PLAYING && <RoundPlaying />}
+      <Text>{partialResults}</Text>
+      {listening ? (
+        <Button
+          title="Stop"
+          onPress={async () => {
+            stop();
+          }}
+        />
+      ) : (
+        <Button
+          title="Start"
+          onPress={async () => {
+            start();
+          }}
+        />
+      )}
     </>
   );
 };
 
 const WrappedApp = () => (
-  <DeviceProvider device={Device.iphone}>
-    <AppProvider>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <App />
-      </SafeAreaView>
-    </AppProvider>
-  </DeviceProvider>
+  <SafeAreaView
+    style={{
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <App />
+  </SafeAreaView>
 );
 
 export default WrappedApp;
