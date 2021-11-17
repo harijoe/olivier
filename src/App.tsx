da448d39e67a505f874a66430683b3950cd81fbe
Julien Vallini <vallinij@gmail.com>
2021-11-17 10:45:59 +0100

    Looks like it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
 import useVoice from 'hooks/useVoice';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, SafeAreaView, Text } from 'react-native';
 
 const App = () => {
   const { start, listening, stop, partialResults, results } = useVoice();
 
+  const [actionable, setActionable] = useState(false);
+
   return (
     <>
       <Text>{partialResults}</Text>
       {listening && <Text>Listening</Text>}
-      {listening ? (
-        <Button
-          title="Stop"
-          onPress={async () => {
-            stop();
-          }}
-        />
-      ) : (
-        <Button
-          title="Start"
-          onPress={async () => {
-            start();
-          }}
-        />
-      )}
+      {listening &&
+        (actionable ? (
+          <Button
+            title="Stop"
+            onPress={async () => {
+              setActionable(false);
+              stop();
+              setTimeout(() => {
+                setActionable(true);
+              }, 2000);
+            }}
+          />
+        ) : (
+          <Text>Loading</Text>
+        ))}
+
+      {!listening &&
+        (actionable ? (
+          <Button
+            title="Start"
+            onPress={async () => {
+              setActionable(false);
+              start();
+              setTimeout(() => {
+                setActionable(true);
+              }, 2000);
+            }}
+          />
+        ) : (
+          <Text>Loading</Text>
+        ))}
     </>
   );
 };
